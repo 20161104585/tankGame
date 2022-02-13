d4a54dd124155964e0b44801da87d981c243b2d1
20161104585 <1663512557@qq.com>
2022-02-13 20:48:23 +0800

    fix##算法学习##链表相关问题补充##

--- src/algorithmLean/giantAlgorithmProblem/Code20220209_01.java
+++ src/algorithmLean/giantAlgorithmProblem/Code20220209_01.java
@@ -207,4 +207,156 @@ public class Code20220209_01 {
         }
         return res;
     }
+
+
+    /**
+     * 给定两个可能有环也可能无环的单链表，头节点head1和head2.
+     * 请实现一个函数，如果两个链表相交，请返回相交的第一个节点。如果不相交，返回null
+     * 【要求】
+     * 如果两个链表长度之和为N，时间复杂度请达到O(N),额外空间复杂度达到O(1)
+     */
+
+    public static Node getIntersectNode(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node loop1 = getLoopNode(head1);
+        Node loop2 = getLoopNode(head2);
+        if (loop1 == null && loop2 == null) {
+            return noLoop(head1, head2);
+        }
+        if (loop1 != null && loop2 != null) {
+            return bothloop(head1, loop1, head2, loop2);
+        }
+        return null;
+    }
+
+    /**
+     * 求给定单链表是否有环，如果有，返回入环结点，如果无，返回null
+     *
+     * @param head
+     * @return
+     */
+    public static Node getLoopNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        //慢指针走一步
+        Node n1 = head.next;
+        //快指针走两步
+        Node n2 = head.next.next;
+        //快慢指针没有相遇。循环
+        while (n1 != n2) {
+            //快指针提前到空，必定无环
+            if (n2.next == null || n2.next.next == null) {
+                return null;
+            }
+            n1 = n1.next;
+            n2 = n2.next.next;
+        }
+        //循环出来后，快慢指针相遇了，重新让快指针回到起点，一次走一步
+        n2 = head;
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return n1;
+    }
+
+    /**
+     * 求两个无环是否相交，相交返回一个相交节点，否则返回null
+     *
+     * @param head1
+     * @return
+     */
+    public static Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+
+        Node cur1 = head1;
+        Node cur2 = head1;
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+
+        //如果最后一个节点不相等，直接返回bull，否则两个链表必定相交
+        if (cur1 != cur2) {
+            return null;
+        }
+        //谁长谁为cur1
+        cur1 = n > 0 ? head1 : head2;
+        //剩下那个为cur2
+        cur2 = cur1 == head1 ? head2 : head1;
+
+        //长链表先走差值步
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        //两个同时向下走，第一个相等的节点就是相交节点，直接返回
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    /**
+     * 两个有环链表，返回第一个相交点，如果不相交返回null
+     *
+     * @param head1
+     * @param loop1
+     * @param head2
+     * @param loop2
+     * @return
+     */
+    public static Node bothloop(Node head1, Node loop1, Node head2, Node loop2) {
+        Node cur1 = null;
+        Node cur2 = null;
+        if (loop1 == loop2) {
+            cur1 = head1;
+            cur2 = head2;
+            int n = 0;
+            while (cur1.next != loop1) {
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2.next != loop1) {
+                n--;
+                cur2 = cur2.next;
+            }
+
+            n = Math.abs(n);
+            cur1 = n > 0 ? head1 : head2;
+            cur2 = cur1 == head1 ? head2 : head1;
+
+            while (n != 0) {
+                n--;
+                cur1 = cur1.next;
+            }
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 == loop2) {
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
+    }
 }
