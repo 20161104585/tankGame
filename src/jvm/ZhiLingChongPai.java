5b8af02f5c581de59064481f99acb1ddae1c14ee
20161104585 <1663512557@qq.com>
2022-03-05 22:19:25 +0800

    fix##jvm调优##指令重排小实验##

---
+++ src/jvm/ZhiLingChongPai.java
@@ -0,0 +1,46 @@
+package jvm;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Description: jvm调优-指令重排
+ * @Author: haole
+ * @Date: 2022/3/5 20:31
+ */
+public class ZhiLingChongPai {
+    public static final int TOTAL = 1_000_000_000;
+    public static int a = 0, b = 0;
+    public static int x = 0, y = 0;
+
+    //验证指令重排
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < TOTAL; i++) {
+            a = 0;
+            b = 0;
+            x = 0;
+            y = 0;
+            CountDownLatch countDownLatch = new CountDownLatch(2);
+
+            new Thread(() -> {
+                a = 1;
+                x = b;
+                countDownLatch.countDown();
+            }).start();
+
+            new Thread(() -> {
+                b = 1;
+                y = a;
+                countDownLatch.countDown();
+
+            }).start();
+
+            countDownLatch.await();
+            System.out.println(x + " " + y + " " + i);
+
+            if (x == 0 && y == 0) {
+                System.out.println("i \t" + i);
+                break;
+            }
+        }
+    }
+}
